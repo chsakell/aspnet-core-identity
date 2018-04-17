10125e607b878e088153fe7e918019a4a0a5bf56
chsakell <chsakell@gmail.com>
2018-04-17 23:27:28 +0300

    add Microsoft.Extensions.Identity.Core package

--- AspNetCoreIdentity/Startup.cs
+++ AspNetCoreIdentity/Startup.cs
@@ -23,6 +23,8 @@ namespace AspNetCoreIdentity
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddIdentityCore<string>(options => { });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
