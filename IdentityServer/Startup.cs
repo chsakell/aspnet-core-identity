13f88cc5a2e399b4b0f74e540019053903708b05
chsakell <chsakell@gmail.com>
2019-02-14 19:37:31 +0200

    add IdentityServer package and services

--- IdentityServer/Startup.cs
+++ IdentityServer/Startup.cs
@@ -15,6 +15,8 @@ namespace IdentityServer
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddIdentityServer()
+            .AddDeveloperSigningCredential();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,10 +27,7 @@ namespace IdentityServer
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseIdentityServer();
         }
     }
 }
